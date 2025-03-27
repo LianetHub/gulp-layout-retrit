72db2814e9c59c746b95563e798047636755bcb9
lianet <jack.tolmak@yandex.ru>
2025-03-27 13:06:57 +0300

    add init commit

---
+++ src/js/app.js
@@ -0,0 +1,308 @@
+"use strict";
+
+// Инициализация Fancybox
+if (typeof Fancybox !== "undefined" && Fancybox !== null) {
+    Fancybox.bind("[data-fancybox]", {
+        dragToClose: false,
+        closeButton: false
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Определение ОС пользователя
+    const isMobile = {
+        Android: () => /Android/i.test(navigator.userAgent),
+        BlackBerry: () => /BlackBerry/i.test(navigator.userAgent),
+        iOS: () => /iPhone|iPad|iPod/i.test(navigator.userAgent),
+        Opera: () => /Opera Mini/i.test(navigator.userAgent),
+        Windows: () => /IEMobile/i.test(navigator.userAgent),
+        any: function () {
+            return this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows();
+        },
+    };
+
+    function getNavigator() {
+        if (isMobile.any() || window.innerWidth < 992) {
+            document.body.classList.remove('_pc');
+            document.body.classList.add('_touch');
+        } else {
+            document.body.classList.remove('_touch');
+            document.body.classList.add('_pc');
+        }
+    }
+
+    getNavigator();
+    window.addEventListener('resize', getNavigator);
+
+    // Обработчик событий
+    document.addEventListener('click', (e) => {
+        if (e.target.closest('.header__menu-toggler')) {
+            toggleMenu();
+        }
+    });
+
+    function toggleMenu() {
+        document.body.classList.toggle('lock');
+        document.querySelector('.header').classList.toggle('open-menu');
+    }
+
+    // Инициализация слайдеров
+    if (document.querySelector('.place__slider')) {
+        new Swiper('.place__slider', {
+            slidesPerView: 1,
+            loop: true,
+            navigation: {
+                nextEl: '.place__next',
+                prevEl: '.place__prev',
+            },
+        });
+    }
+
+    if (document.querySelector('.reviews__slider')) {
+        new Swiper('.reviews__slider', {
+            slidesPerView: "auto",
+            spaceBetween: 10,
+            breakpoints: {
+                767.98: {
+                    spaceBetween: 20,
+                },
+                991.98: {
+                    spaceBetween: 30,
+                },
+                1199.98: {
+                    spaceBetween: 40,
+                }
+            }
+
+        });
+    }
+
+
+    // spollers
+
+    const spollersArray = document.querySelectorAll("[data-spollers]");
+    if (spollersArray.length > 0) {
+
+        const spollersRegular = Array.from(spollersArray).filter(function (
+            item,
+            index,
+            self
+        ) {
+            return !item.dataset.spollers.split(",")[0];
+        });
+
+        if (spollersRegular.length > 0) {
+            initSpollers(spollersRegular);
+        }
+
+        const spollersMedia = Array.from(spollersArray).filter(function (
+            item,
+            index,
+            self
+        ) {
+            return item.dataset.spollers.split(",")[0];
+        });
+
+        if (spollersMedia.length > 0) {
+            const breakpointsArray = [];
+            spollersMedia.forEach((item) => {
+                const params = item.dataset.spollers;
+                const breakpoint = {};
+                const paramsArray = params.split(",");
+                breakpoint.value = paramsArray[0];
+                breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : "max";
+                breakpoint.item = item;
+                breakpointsArray.push(breakpoint);
+            });
+
+
+            let mediaQueries = breakpointsArray.map(function (item) {
+                return (
+                    "(" +
+                    item.type +
+                    "-width: " +
+                    item.value +
+                    "px)," +
+                    item.value +
+                    "," +
+                    item.type
+                );
+            });
+            mediaQueries = mediaQueries.filter(function (item, index, self) {
+                return self.indexOf(item) === index;
+            });
+
+
+            mediaQueries.forEach((breakpoint) => {
+                const paramsArray = breakpoint.split(",");
+                const mediaBreakpoint = paramsArray[1];
+                const mediaType = paramsArray[2];
+                const matchMedia = window.matchMedia(paramsArray[0]);
+
+                const spollersArray = breakpointsArray.filter(function (item) {
+                    if (item.value === mediaBreakpoint && item.type === mediaType) {
+                        return true;
+                    }
+                });
+
+                if (matchMedia.removeEventListener) {
+                    matchMedia.removeEventListener("change", () => {
+                        initSpollers(spollersArray, matchMedia);
+                    });
+                }
+                initSpollers(spollersArray, matchMedia);
+            });
+        }
+
+
+        function initSpollers(spollersArray, matchMedia = false) {
+            spollersArray.forEach((spollersBlock) => {
+                spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
+                if (matchMedia.matches || !matchMedia) {
+                    spollersBlock.classList.add("_init");
+                    initSpollerBody(spollersBlock);
+                    spollersBlock.addEventListener("click", setSpollerAction);
+                } else {
+                    spollersBlock.classList.remove("_init");
+                    initSpollerBody(spollersBlock, false);
+                    spollersBlock.removeEventListener("click", setSpollerAction);
+                }
+            });
+        }
+
+        function initSpollerBody(spollersBlock, hideSpollerBody = true) {
+            const spollerTitles = spollersBlock.querySelectorAll("[data-spoller]");
+            if (spollerTitles.length > 0) {
+                spollerTitles.forEach((spollerTitle) => {
+                    if (hideSpollerBody) {
+                        spollerTitle.removeAttribute("tabindex");
+                        if (!spollerTitle.classList.contains("_active")) {
+                            spollerTitle.nextElementSibling.hidden = true;
+                        }
+                    } else {
+                        spollerTitle.setAttribute("tabindex", "-1");
+                        spollerTitle.nextElementSibling.hidden = false;
+                    }
+                });
+            }
+        }
+
+        function setSpollerAction(e) {
+            const el = e.target;
+            if (el.hasAttribute("data-spoller") || el.closest("[data-spoller]")) {
+                const spollerTitle = el.hasAttribute("data-spoller")
+                    ? el
+                    : el.closest("[data-spoller]");
+                const spollersBlock = spollerTitle.closest("[data-spollers]");
+                const oneSpoller = spollersBlock.hasAttribute("data-one-spoller")
+                    ? true
+                    : false;
+                if (!spollersBlock.querySelectorAll("._slide").length) {
+                    if (oneSpoller && !spollerTitle.classList.contains("_active")) {
+                        hideSpollersBody(spollersBlock);
+                    }
+                    spollerTitle.classList.toggle("_active");
+                    spollerTitle.nextElementSibling.slideToggle(300);
+                }
+                e.preventDefault();
+            }
+        }
+
+        function hideSpollersBody(spollersBlock) {
+            const spollerActiveTitle = spollersBlock.querySelector(
+                "[data-spoller]._active"
+            );
+            if (spollerActiveTitle) {
+                spollerActiveTitle.classList.remove("_active");
+                spollerActiveTitle.nextElementSibling.slideUp(300);
+            }
+        }
+    }
+
+
+})
+
+
+
+
+
+//  init slide toggle
+
+HTMLElement.prototype.slideToggle = function (duration, callback) {
+    if (this.clientHeight === 0) {
+        _s(this, duration, callback, true);
+    } else {
+        _s(this, duration, callback);
+    }
+};
+
+HTMLElement.prototype.slideUp = function (duration, callback) {
+    _s(this, duration, callback);
+};
+
+HTMLElement.prototype.slideDown = function (duration, callback) {
+    _s(this, duration, callback, true);
+};
+
+function _s(el, duration, callback, isDown) {
+
+    if (typeof duration === 'undefined') duration = 400;
+    if (typeof isDown === 'undefined') isDown = false;
+
+    el.style.overflow = "hidden";
+    if (isDown) el.style.display = "block";
+
+    var elStyles = window.getComputedStyle(el);
+
+    var elHeight = parseFloat(elStyles.getPropertyValue('height'));
+    var elPaddingTop = parseFloat(elStyles.getPropertyValue('padding-top'));
+    var elPaddingBottom = parseFloat(elStyles.getPropertyValue('padding-bottom'));
+    var elMarginTop = parseFloat(elStyles.getPropertyValue('margin-top'));
+    var elMarginBottom = parseFloat(elStyles.getPropertyValue('margin-bottom'));
+
+    var stepHeight = elHeight / duration;
+    var stepPaddingTop = elPaddingTop / duration;
+    var stepPaddingBottom = elPaddingBottom / duration;
+    var stepMarginTop = elMarginTop / duration;
+    var stepMarginBottom = elMarginBottom / duration;
+
+    var start;
+
+    function step(timestamp) {
+
+        if (start === undefined) start = timestamp;
+
+        var elapsed = timestamp - start;
+
+        if (isDown) {
+            el.style.height = (stepHeight * elapsed) + "px";
+            el.style.paddingTop = (stepPaddingTop * elapsed) + "px";
+            el.style.paddingBottom = (stepPaddingBottom * elapsed) + "px";
+            el.style.marginTop = (stepMarginTop * elapsed) + "px";
+            el.style.marginBottom = (stepMarginBottom * elapsed) + "px";
+        } else {
+            el.style.height = elHeight - (stepHeight * elapsed) + "px";
+            el.style.paddingTop = elPaddingTop - (stepPaddingTop * elapsed) + "px";
+            el.style.paddingBottom = elPaddingBottom - (stepPaddingBottom * elapsed) + "px";
+            el.style.marginTop = elMarginTop - (stepMarginTop * elapsed) + "px";
+            el.style.marginBottom = elMarginBottom - (stepMarginBottom * elapsed) + "px";
+        }
+
+        if (elapsed >= duration) {
+            el.style.height = "";
+            el.style.paddingTop = "";
+            el.style.paddingBottom = "";
+            el.style.marginTop = "";
+            el.style.marginBottom = "";
+            el.style.overflow = "";
+            if (!isDown) el.style.display = "none";
+            if (typeof callback === 'function') callback();
+        } else {
+            window.requestAnimationFrame(step);
+        }
+    }
+
+    window.requestAnimationFrame(step);
+}
